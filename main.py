fecd7f5ae3d92abf4c2e8532b3244ae1234709d2
nibble90 <18497547+nibble90@users.noreply.github.com>
2022-02-25 23:26:13 +0000

    Added top 10 bbc news articles

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ from escpos import printer
 from os import getenv
 from dotenv import load_dotenv
 from pyowm import OWM
+from sys import exit
+from newsapi import NewsApiClient
 
 class ReceiptWeather:
-    def __init__(self):
+    def __init__(self, dummy=False):
         try:
             load_dotenv(dotenv_path=".env")
             self.owm = OWM(getenv('OPENWEATHERAPIKEY'))
             self.city_id = int(getenv('WEATHERID'))
+            self.newsAPI = NewsApiClient(api_key=getenv('NEWSAPIKEY'))
+            self.print_array = []
 
             self.mgr = self.owm.weather_manager()
-                        
-            # self.p = printer.Usb(0x2730, 0x0fff, 0, 0x81, 0x02)
-            self.p = printer.Dummy()
+              
+            self.p = printer.Usb(0x076c, 0x0303, 0, 0, 0x02) if not dummy else printer.Dummy()
             self.observation = self.mgr.weather_at_id(self.city_id)
             # Bus 002 Device 001: ID 04b8:0202 Epson ...
             # lsusb -vvv -d xxxx:xxxx | grep iInterface
@@ -24,6 +27,7 @@ class ReceiptWeather:
             # OR = Usb.(0x04b8, 0x0202)
         except Exception as e:
             print(e)
+            exit(1)
 
     def get_weather(self):
         message = "{}".format(self.observation.weather.detailed_status)
@@ -32,27 +36,41 @@ class ReceiptWeather:
     def get_temperature(self):
         # {'temp', 'temp_max', 'temp_min', 'feels_like', 'temp_kf'}
         info = self.observation.weather.temperature('celsius')
-        message = [
+        messages = [
         "Current Temperature : {}".format(info['temp']),
         "Feels Like          : {}".format(info['feels_like']),
         "Maximum Temperature : {}".format(info['temp_max']),
         "Lowest Temperature  : {}".format(info['temp_min'])]
-        return message
+        for message in messages: self.print_array.append(message)
 
     def get_header(self):
-        message = "Weather at: {}".format(self.observation.reception_time('date').time())
-        return message
+        messages = [
+            "Weather at: {}".format(self.observation.reception_time('date').time()),
+            "--------------------",
+            "Condition: {}".format(self.get_weather())]
+        for message in messages: self.print_array.append(message)
+
+    def get_top_5_news(self):
+        messages = self.newsAPI.get_top_headlines(sources='bbc-news')['articles']
+        self.print_array.append("--------------------")
+        for message in messages: messages = self.print_array.append(message['title'])
 
     def assemble_print(self):
-        self.p.text("{}\n".format(self.get_header()))
-        self.p.text("--------------------\n")
-        self.p.text("Condition: {}\n".format(self.get_weather()))
-        for message in self.get_temperature():
-            self.p.text("{}\n".format(message))
-        self.p.cut()
+        try:
+            self.get_header()
+            self.get_temperature()
+            self.get_top_5_news()
+            for message in self.print_array:
+                self.p.text("{}\n".format(message))
+            self.p.cut(mode='PART')
+        except Exception as e:
+            print(e)
+            exit(1)
 
 if __name__ == "__main__":
-    obj = ReceiptWeather()
+    dummy = True
+    obj = ReceiptWeather(dummy=dummy)
     obj.assemble_print()
-    dummyoutput = obj.p.output
-    print(dummyoutput.decode("utf-8"))
+    if(dummy):
+        print(obj.p.output.decode("utf-8"))
+    print("done")
